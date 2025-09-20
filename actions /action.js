58b055faa0528a6000582a43057308c8444b0e3e
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-09-20 16:41:54 +0330

    Uploading & Storing Images

--- actions /action.js
+++ actions /action.js
@@ -1,37 +1,46 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
 
-  
 export async function createPost(prevState, formData) {
-    
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
 
-    let errors = [];
+  let errors = [];
 
-    if (!title || title.trim().lenght === 0) {
-      errors.push("Title is required");
-    }
-    if (!content || content.trim().lenght === 0) {
-      errors.push("content is required");
-    }
-    if (!image || image.size === 0) {
-      errors.push("image is required");
-    }
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed"), revalidatePath("/feed");
+  if (!title || title.trim().lenght === 0) {
+    errors.push("Title is required");
   }
+  if (!content || content.trim().lenght === 0) {
+    errors.push("content is required");
+  }
+  if (!image || image.size === 0) {
+    errors.push("image is required");
+  }
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  let imageUrl ;
+
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error){
+    throw new Error(
+      "image upload faild , post was not created . please try again later"
+    );
+  }
+
+  await storePost({
+    imageUrl,
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed"), revalidatePath("/feed");
+}
--- app/new-post/page.js
+++ app/new-post/page.js
@@ -3,8 +3,5 @@ import PostForm from "@/components/post-form";
 import {createPost} from "@/actions /action";
 
 export default function NewPostPage() {
-  
-  
-
   return <PostForm action={createPost} />;
 }
